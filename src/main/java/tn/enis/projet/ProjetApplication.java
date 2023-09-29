e716f455738950bd9f85f06cc5fd97c86a95647c
Takoua Jradi <takoua.jradi@enis.tn>
2023-09-29 16:20:26 +0100

    first commit after modification

--- src/main/java/tn/enis/projet/ProjetApplication.java
+++ src/main/java/tn/enis/projet/ProjetApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class ProjetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplication.class, args);
+		treazgkznsz
 	}
 
 }
